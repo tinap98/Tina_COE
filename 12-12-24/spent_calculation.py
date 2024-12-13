126021a19bc245f47a0fc588c70121ed2ef003a7
tinap98 <22b81a05bs.cvr@gmail.com>
2024-12-13 21:49:29 +0530

    added 12-12-24 files again via cmd

--- 12-12-24/spent_calculation.py
+++ 12-12-24/spent_calculation.py
@@ -3,7 +3,7 @@ def calculate_percentage(salary, bill1, bill2, bill3):
     percentage_spent = (total_bills / salary) * 100
     return percentage_spent
 
-
+#billing inputs
 salary = float(input("Enter your salary: "))
 bill1 = float(input("Enter your bill amount: "))
 bill2 = float(input("Enter your bill amount: "))
